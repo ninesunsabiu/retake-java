ec0f3610fc69aeb72b7f68c273a46fcea6524424
Ezio Lin <ninesuns.lin@outlook.com>
2024-04-13 00:40:55 +0800

    ♻️ 优化代码表达
    - 对静态常量 使用 SCREAMING_SNAKE_CASE 命名
    - Stream#reduce 的调用更换为 Stream#collect 来最终输出结果

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,41 +1,27 @@
 import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 class SqueakyClean {
-    private static final char spaceChar = ' ';
-    private static final Map<Character, Character> leetSpeakTable = Map.of(
+    private static final char UNDERSCORE = '_';
+    private static final char KEBAB = '-';
+
+    private static final Map<Character, Character> REPLACE_MAP = Map.of(
             '4', 'a',
             '3', 'e',
             '0', 'o',
             '1', 'l',
-            '7', 't'
+            '7', 't',
+            ' ', UNDERSCORE
     );
-    private static final char kebab = '-';
-    private static final char underscore = '_';
 
-    static class Result {
-
-        private boolean meetKebeb = false;
-        private Stream<Character> sb = Stream.empty();
-
-        public Result() {}
-
-        protected Result(boolean meetKebeb, Stream<Character> sb) {
-            this.meetKebeb = meetKebeb;
-            this.sb = sb;
-        }
-
-        public static Result append(Result acc, char c) {
-            if (c == kebab) {
-                return new Result(true, acc.sb);
-            } else {
-                return new Result(
-                        false,
-                        Stream.concat(acc.sb, Stream.of(acc.meetKebeb ? Character.toUpperCase(c) : c))
-                );
-            }
-        }
+    private static boolean isPreserveChar(char c) {
+        return Character.isLetter(c) || c == UNDERSCORE || c == KEBAB;
     }
 
     static String clean(String identifier) {
@@ -44,27 +30,49 @@ class SqueakyClean {
         return IntStream.range(0, charArray.length)
                 .mapToObj(i -> charArray[i])
 
-                // Replace any spaces encountered with underscores
-                .map(it -> it == spaceChar ? underscore : it)
-
-                // Convert kebab-case to camelCase
-                .reduce(
-                        new Result(),
-                        Result::append,
-                        (result1, result2) -> result2
-                )
-                .sb
-                // Convert leetspeak to normal text
-                .map(it -> leetSpeakTable.getOrDefault(it, it))
+                // Convert to normal text
+                .map(it -> REPLACE_MAP.getOrDefault(it, it))
 
                 // Omit characters that are not letters
-                .filter(it -> Character.isLetter(it) || it == underscore)
-                .collect(
-                        StringBuilder::new,
-                        StringBuilder::append,
-                        StringBuilder::append
-                )
-                .toString();
+                .filter(SqueakyClean::isPreserveChar)
 
+                // Convert kebab-case to camelCase
+                .collect(new Collector<Character, StringBuilder, String>() {
+                    private boolean meetKebab = false;
+
+                    @Override
+                    public Supplier<StringBuilder> supplier() {
+                        return StringBuilder::new;
+                    }
+
+                    @Override
+                    public BiConsumer<StringBuilder, Character> accumulator() {
+                        return (sb, c) -> {
+                            if (c == KEBAB) {
+                                // meet kebab skip this round and set mark
+                                meetKebab = true;
+                            } else {
+                                // normal append this character and set mark to false
+                                sb.append(meetKebab ? Character.toUpperCase(c) : c);
+                                meetKebab = false;
+                            }
+                        };
+                    }
+
+                    @Override
+                    public BinaryOperator<StringBuilder> combiner() {
+                        return StringBuilder::append;
+                    }
+
+                    @Override
+                    public Function<StringBuilder, String> finisher() {
+                        return StringBuilder::toString;
+                    }
+
+                    @Override
+                    public Set<Characteristics> characteristics() {
+                        return Set.of();
+                    }
+                });
     }
 }
